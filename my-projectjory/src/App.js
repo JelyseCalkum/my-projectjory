068f98e4672058b03da267f68ebcb00ce5242656
jelysecalkum <jelyse.lauren@gmail.com>
2023-03-25 16:50:23 -0700

    added footer

--- my-projectjory/src/App.js
+++ my-projectjory/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './App.css';
-import 'bootstrap/dist/bootstrap.min.css';
+//import 'bootstrap/dist/bootstrap.min.css';
 
 function App() {
   return (
