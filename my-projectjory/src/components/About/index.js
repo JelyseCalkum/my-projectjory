32d37dcfd458550846c8acd05c6dffae2084d668
jelysecalkum <jelyse.lauren@gmail.com>
2023-03-25 18:04:58 -0700

    added content for header about nav footer proj and portf

---
+++ my-projectjory/src/components/About/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function About() {
+
+    return(
+        <section className="container">
+            <h2 class="top-title">Jelyse Calkum</h2>
+            <hr></hr>
+            <div>
+                <img class="mb-5" id="avatar" src="my-projectjory\src\assets\images\meandhenry.jpeg" alt="Jelyse Calkum"/>
+            <p>Programming student blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</p>
+            <p>blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah </p>
+            <p>blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah </p>
+            
+            </div>
+            </section>
+    )
+}
+
+export default About;
