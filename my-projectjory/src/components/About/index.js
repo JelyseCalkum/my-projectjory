b5bfa135843fefc82d2435e151a5aa6939035ada
jelysecalkum <jelyse.lauren@gmail.com>
2023-03-26 14:31:50 -0700

    added projects to portfolio

--- my-projectjory/src/components/About/index.js
+++ my-projectjory/src/components/About/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import profileImage from "../../assets/images/meandhenry.jpeg";
 
 function About() {
 
@@ -7,7 +8,7 @@ function About() {
             <h2 class="top-title">Jelyse Calkum</h2>
             <hr></hr>
             <div>
-                <img class="mb-5" id="avatar" src="my-projectjory\src\assets\images\meandhenry.jpeg" alt="Jelyse Calkum"/>
+                <img class="mb-5" id="avatar" src={profileImage} alt="Jelyse Calkum"/>
             <p>Programming student blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</p>
             <p>blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah </p>
             <p>blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah </p>
