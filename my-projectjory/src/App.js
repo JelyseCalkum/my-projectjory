8eae0a667f250e75b4d3e000d1c3b302bebd9eed
jelysecalkum <jelyse.lauren@gmail.com>
2023-03-27 13:58:27 -0700

    imported About to app.js

--- my-projectjory/src/App.js
+++ my-projectjory/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import About from './components/About';
 
 function App() {
   return (
     <div>
-      <Header></Header>
-      <Footer></Footer>
+      <Header />
+      <About />
+      <Footer />
     </div>
   );
 }
