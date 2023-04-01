c4ee1bebbe44d932344cee3b65a421cf364e9c57
jelysecalkum <jelyse.lauren@gmail.com>
2023-04-01 14:22:37 -0700

    fixed about me temporarily

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,16 +7,15 @@ function About() {
         <section className="container">
             <h2 class="top-title">Jelyse (Jory) Calkum</h2>
             <hr></hr>
-            <div>
+            <div id="about-me">
                 <img class="mb-5" id="avatar" src={profileImage} alt="Jelyse Calkum"/>
 
-            <p>Hi, my name is Jelyse, but you can call me Jory. I'm currently working towards becoming a full-stack web developer.</p>
+            <p>Hi, my name is Jelyse, but you can call me Jory. I'm currently working towards becoming a full-stack web developer.
+            When I'm not dedicating my time to studying, you can catch me engaging in an epic Nerf gun battle with my five-year-old son,
+            assisting him in building the ultimate Hot Wheels track, or passionately discussing Star Wars.
 
-            <p>When I'm not dedicating my time to studying, you can catch me engaging in an epic Nerf gun battle with my five-year-old son,
-              assisting him in building the ultimate Hot Wheels track, or passionately discussing Star Wars.</p>
-
-            <p>Currently, my primary focus is on expanding my knowledge of full-stack web development, so that I can utilize my 
-              strong attention to detail and efficiency to pursue a career where I can express my creativity to the fullest extent.</p>
+            Currently, my primary focus is on expanding my knowledge of full-stack web development, so that I can utilize my 
+            strong attention to detail and efficiency to pursue a career where I can express my creativity to the fullest extent.</p>
             
             </div>
             </section>
